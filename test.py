b810ec60d5aa18d7d7642c82f036b8c6f72a6693
k-zehnder <kjzehnder3@gmail.com>
2021-07-21 18:26:25 -0700

    add test to check that index returns "hello world" passed

--- test.py
+++ test.py
@@ -16,6 +16,6 @@ class HomeTestCase(unittest.TestCase):
     tester = app.test_client(self)
     response = tester.get('/', content_type='application/json')
     self.assertEqual(response.status_code, 200)
-
+    self.assertEqual(response.data, b"Hello World!")
 if __name__ == '__main__':
     unittest.main()
