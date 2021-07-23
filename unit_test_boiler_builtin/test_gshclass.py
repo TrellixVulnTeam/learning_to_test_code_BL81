01a35af27a90b70c8a289c0ae0dca0619bec47ce
kevin <righttrianglesrkewl3@gmail.com>
2021-07-23 16:40:11 -0700

    hide creds

--- unit_test_boiler_builtin/test_gshclass.py
+++ unit_test_boiler_builtin/test_gshclass.py
@@ -1,12 +1,13 @@
 from gshclass import *
 import pytest
 import os
+from dotenv import load_dotenv
+
+# creds
+load_dotenv()
+cred_path = os.environ.get("CRED_PATH")
 
 # testing constructor of GoogleSheetHelper class
-cred_path = "/home/batman/Desktop/learning_to_test_code/unit_test_boiler_builtin/master_key.json"
-#cred_path = os.environ.get("CRED_JSON")
-
-# test constructor
 def test_make_gsh_helper():
     gsh1 = GoogleSheetHelper(cred_path, "google_postgres", "existing")
     assert gsh1.cred_json == cred_path
