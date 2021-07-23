b8cc77bdcc314f212c48a8baf12f32853f5d9a71
k-zehnder <kjzehnder3@gmail.com>
2021-07-22 17:32:00 -0700

    changed file structure more testds

--- flask/test.py
+++ flask/test.py
@@ -17,5 +17,6 @@ class HomeTestCase(unittest.TestCase):
     response = tester.get('/', content_type='application/json')
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.data, b"Hello World!")
+
 if __name__ == '__main__':
     unittest.main()
