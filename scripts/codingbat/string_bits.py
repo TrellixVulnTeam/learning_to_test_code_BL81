53458b7038f3aa15b0816cf387aefa80e4916f67
kevin <righttrianglesrkewl3@gmail.com>
2021-08-06 10:31:48 -0700

    more functions pass unit tests

--- scripts/codingbat/string_bits.py
+++ scripts/codingbat/string_bits.py
@@ -6,3 +6,6 @@ def string_bits(str):
     for i in range(0, len(str), 2):
         output_string += str[i]
     return output_string
+
+def string_bits_refactor1(str):
+    return "".join(str[i] for i in range(0, len(str), 2))
--- tests/codingbat/test_string_bits.py
+++ tests/codingbat/test_string_bits.py
@@ -1,7 +1,11 @@
-from scripts.codingbat import string_bits
+from scripts.codingbat import string_bits # string_bits is file
 
 def test_return_string():
     assert string_bits.return_string("z") == "z"
 
 def test_string_bits():
     assert string_bits.string_bits("Hello") == "Hlo"
+
+    # must start function name as test_ or wont work (e.g. string_bits_refactor1 must be test_string_bits_refactor1)
+def test_string_bits_refactor1():
+    assert string_bits.string_bits_refactor1("Hello") == "Hlo"
