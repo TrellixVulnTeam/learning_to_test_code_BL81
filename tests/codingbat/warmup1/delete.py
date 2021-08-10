0410530e0f26e3ed5d00d7ffc9261c7529b6f0f9
kevin <righttrianglesrkewl3@gmail.com>
2021-08-10 09:32:06 -0700

    cleanup

---
+++ tests/codingbat/warmup1/delete.py
@@ -0,0 +1,6 @@
+import unittest
+from scripts.codingbat.warmup2 import array_count # array_count9 = file
+
+# reminder: function name must begin with test_*
+def test_array_count9():
+    assert array_count.array_count9([1, 2, 9]) == 1
