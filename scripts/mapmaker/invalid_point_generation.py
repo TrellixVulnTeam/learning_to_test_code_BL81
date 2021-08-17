9301c710d061aeab5b553b0f524c94fb5b80fa1e
Kevin Z <righttrianglesrkewl3@github.com>
2021-08-16 18:33:29 -0700

    cleanup2

--- scripts/mapmaker/invalid_point_generation.py
+++ scripts/mapmaker/invalid_point_generation.py
@@ -1,16 +1,12 @@
 
 class Point:
     def __init__(self, city, lat, long):
-        if not (-90 <= lat <= 90) or not (-180 <= long <= 180):
-            raise ValueError("Invalid latitude, longitude combination")
-    
         self.lat = lat
         self.long = long
         self.city = city
-    
-        #     self.lat = lat
-        #     self.long = long
 
+        if not (-90 <= lat <= 90) or not (-180 <= long <= 180):
+            raise ValueError("Invalid latitude, longitude combination")
 
     def get_lat_long(self):
         return (self.lat, self.long)
