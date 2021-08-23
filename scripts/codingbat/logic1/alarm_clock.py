00b4a3a7139464b2bf583c5da93d27792a40a192
kevin <righttrianglesrkewl3@gmail.com>
2021-08-23 11:29:01 -0700

    alarm clock

--- scripts/codingbat/logic1/alarm_clock.py
+++ scripts/codingbat/logic1/alarm_clock.py
@@ -6,29 +6,35 @@ alarm_clock(1, False) → '7:00'
 alarm_clock(5, False) → '7:00'
 alarm_clock(0, False) → '10:00'
 """
-mappings = {
-        0 : "Sunday",
-        1 : "Monday",
-        2 : "Tuesday",
-        3 : "Wednesday",
-        4 : "Thursday",
-        5 : "Friday",
-        6 : "Saturday",
-}
 
-vacation = True
-day = mappings[6]
+def alarm_clock(day, vacation):
+    mappings = {
+    0 : "Sunday",
+    1 : "Monday",
+    2 : "Tuesday",
+    3 : "Wednesday",
+    4 : "Thursday",
+    5 : "Friday",
+    6 : "Saturday",
+    }
 
-if vacation:
-    if (day != mappings[0]) and (day != mappings[6]): # not in
-        wakeup = "10:00"
+    if vacation:
+        print(f"on vacation")
+
+        if (mappings[day] == "Saturday") or (mappings[day] == "Sunday"):
+            wakeup = "off"
+            return wakeup
+        else:
+            wakeup = "10:00"
+            return wakeup
+    
     else:
-        wakeup = "off"
-elif not vacation:
-    print("Not on vacation")
-    if (day != mappings[0]) and (day != mappings[6]):
-        wakeup = "7:00"
-    else:
-        wakeup = "10:00"
+        print(f"not on vacation")
+        if (mappings[day] == "Saturday") or (mappings[day] == "Sunday"):
+            wakeup = "10:00"
+            return wakeup
+        else:
+            wakeup = "7:00"
+            return wakeup
 
-print(f"You should wakeup at: {wakeup}")
+print(alarm_clock(0, False))
