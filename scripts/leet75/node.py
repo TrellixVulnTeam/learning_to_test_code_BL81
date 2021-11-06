82aa132b75d3e2d7ef8db1174bc910533c9d6bde
plusplusaviator <plusplusaviator@sp4.hsd1.ca.comcast.net>
2021-11-06 12:33:28 -0700

    simple node with tests added

--- scripts/leet75/node.py
+++ scripts/leet75/node.py
@@ -1,21 +1,14 @@
-# basic binary tree
-from typing import List, Dict
+from typing import Dict, List
 
 class Node:
     def __init__(self, data: int) -> None:
         self.data = data
         self.left = None
         self.right = None
-
+    
     def __str__(self):
         return str(self.data)
 
-def create_tree():
-    node0 = Node(0)
-    node0.left = Node(1)
-    node0.right = Node(2)
-    return node0
-
 if __name__ == "__main__":
-    node0 = create_tree()
+    node0 = Node(0)
     print(node0)
--- tests/leet75/test_basic_binary_tree.py
+++ tests/leet75/test_basic_binary_tree.py
@@ -8,16 +8,3 @@ class Test:
         [Node(1), 1]
     ]
 
-    # returns root node of built-tree
-    node0 = sample_tree()
-
-    def test_node_str(self):
-        node = Node(0)
-        assert type(node.__str__()) == str
-        assert node.__str__() == str(node.data)
-
-    def test_sample_tree(self):
-        assert [0, 2, 1] == [self.node0.data, self.node0.right.data, self.node0.left.data]
-
-    def test_traversal_inorder(self):
-        assert [3, 1, 4, 0, 5, 2, 6] == self.node0.traverse_inorder()
