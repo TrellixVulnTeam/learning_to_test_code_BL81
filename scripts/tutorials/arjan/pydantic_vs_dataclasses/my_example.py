5ebb4fad7619bf132622cc2ec50e0c4caea8e3bc
k-zehnder <kjzehnder3@gmail.com>
2022-01-26 15:15:37 -0800

    zombie cleanup

--- scripts/tutorials/arjan/pydantic_vs_dataclasses/my_example.py
+++ scripts/tutorials/arjan/pydantic_vs_dataclasses/my_example.py
@@ -48,28 +48,12 @@ class StockData:
         await asyncio.gather(*self.tasks)
         return self.results
 
-    def get_and_save_json(self, ticker: str) -> None:
-        ticker = Ticker(ticker)
-        url = self.API_PATTERN.format(ticker)
-        
-        # request data (text)
-        r = httpx.get(url)
-        print(type(r.text)) # str
-        print(type(r.json())) # dict
-        
-        # print("-", type(json.loads(r.text))) # dict
-        # print("-", json.loads(r.text))
-        
-        # save dict to disk
-        with open(self.path, "w") as outfile:
-            json.dump(r.json(), outfile)
-
     def main(self) -> None:
         """Main function."""
 
         # Read data from a JSON file
         with open("scripts/tutorials/arjan/pydantic_vs_dataclasses/aapl_fb.json") as file:
-            data = json.load(file) # dict
+            data = json.load(file) # loads as dict
             tickers: List[Ticker] = [Ticker.custom_init(item["ticker"]) for item in data["data"]]
             print(tickers[0].name)
         return tickers
