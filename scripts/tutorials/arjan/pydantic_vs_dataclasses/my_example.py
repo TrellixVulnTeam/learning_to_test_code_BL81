47b2a5b8378b2d5384e48ef463aae13052eff053
k-zehnder <kjzehnder3@gmail.com>
2022-01-26 17:31:21 -0800

    added validator for appl only and custom validator

--- scripts/tutorials/arjan/pydantic_vs_dataclasses/my_example.py
+++ scripts/tutorials/arjan/pydantic_vs_dataclasses/my_example.py
@@ -7,7 +7,7 @@ from importlib.resources import path
 import json
 from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator
 
 from hypothesis import given
 from hypothesis.strategies import from_type
@@ -18,6 +18,12 @@ import httpx
 
 class Ticker(BaseModel):
     name: str
+    
+    @validator('name')
+    def value_must_equal_aapl(cls, v):
+        if v != "AAPL":
+            raise ValueError("value must be 'AAPL'")
+        return v
     
     @classmethod
     def custom_init(cls, name: str):
@@ -46,26 +52,44 @@ class StockData:
             ticker = Ticker.custom_init(ticker)
             self.tasks.append(self.get_ticker_async(ticker))
         await asyncio.gather(*self.tasks)
+        
+        with open('scripts/tutorials/arjan/pydantic_vs_dataclasses/pretty_data.json', 'w', encoding='utf-8') as f:
+            data = {"results" : self.results}
+            json.dump(data, f, ensure_ascii=False, indent=4)
         return self.results
 
     def main(self) -> None:
         """Main function."""
 
         # Read data from a JSON file
-        with open("scripts/tutorials/arjan/pydantic_vs_dataclasses/aapl_fb.json") as file:
+        with open("scripts/tutorials/arjan/pydantic_vs_dataclasses/pretty_data.json") as file:
             data = json.load(file) # loads as dict
-            tickers: List[Ticker] = [Ticker.custom_init(item["ticker"]) for item in data["data"]]
-            print(tickers[0].name)
-        return tickers
+            tickers: List[Ticker] = [Ticker.custom_init(item["ticker"]) for item in data["results"]]
+
+            # tickers: List[Ticker] = [Ticker(**item) for item in data]
+            # print(tickers[0].name)
+            return tickers
+            
+    def demo_errors(self) -> None:
+        # Read data from a JSON file
+        with open("scripts/tutorials/arjan/pydantic_vs_dataclasses/pretty_data.json") as file:
+            data = json.load(file) # loads as dict
+            
+        for item in data["results"]:           
+            try:
+                print(Ticker.custom_init(item["ticker"]))
+            except ValidationError as e:
+                print(e.errors())        
+    
     
 
 if __name__ == "__main__":
     
-    sd = StockData("scripts/tutorials/arjan/pydantic_vs_dataclasses/aapl_fb.json")
+    sd = StockData("scripts/tutorials/arjan/pydantic_vs_dataclasses/pretty_data.json")
     
-    res = sd.main()
-    print(res)    
-    
+    # res = sd.main()
+    # print(res)
+    sd.demo_errors()    
     
     # THIS IS FOR GETTING DATA
     # print(asyncio.run(sd.gather_async()))
