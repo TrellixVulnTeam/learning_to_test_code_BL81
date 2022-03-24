030626ad32a8b63362613fe3ef0db59fdb60042d
Kevin Z <righttrianglesrkewl3@github.com>
2022-03-24 09:55:34 -0700

    added test case to verify exception raised when list has exhausted all elements

--- tests/tutorials/snippets/test_iterator_class.py
+++ tests/tutorials/snippets/test_iterator_class.py
@@ -1,7 +1,8 @@
 from scripts.tutorials.snippets.iterator_class import ListIterator
+import unittest
+import pytest
 
-
-class Test:
+class Test(unittest.TestCase):
 
     def test_next(self):
         a_list = [1, 2, 3, 4]
@@ -10,4 +11,10 @@ class Test:
             assert a_list[idx] == obj.__next__()
     
     def test_raise_stop_iteration_exception(self):
-        pass
+        a_list = [1, 2, 3, 4]
+        obj = ListIterator(a_list)
+        for idx, _ in enumerate(a_list):
+            if idx != len(a_list):
+                assert a_list[idx] == obj.__next__()
+            else:
+                self.assertRaises(StopIteration, obj.__next__(), a_list[idx])
