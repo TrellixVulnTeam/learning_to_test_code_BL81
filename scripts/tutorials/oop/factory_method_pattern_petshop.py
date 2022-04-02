614d2e08360faa0000be490e4f0c5677b8e261fb
Kevin Z <kjzehnder3@gmail.com>
2022-04-02 14:35:03 -0700

    change variable names to make more sense

--- scripts/tutorials/oop/factory_method_pattern_petshop.py
+++ scripts/tutorials/oop/factory_method_pattern_petshop.py
@@ -7,7 +7,7 @@ class Creator:
     def factory_method(self):
         pass
 
-    def some_operation(self):
+    def buy_pet(self):
         # sourcery skip: inline-immediately-returned-variable
         product = self.factory_method()
         result = f"Creator: The same creator's code has just worked with {product.speak()}"        
@@ -37,7 +37,7 @@ class Cat(Pet):
 
 def client_code(creator: Creator):
     print(f"Client: I'm not aware of the creator's class, but it still works.\n"
-    f"{creator.some_operation()}", end="")
+    f"{creator.buy_pet()}", end="")
 
 if __name__ == '__main__':
     creators = [ConcreteCatShop(), ConcreteDogShop()]
