2ce5a4a33894378feeeca429fe61741c5e987f9e
Kevin Z <kjzehnder3@gmail.com>
2022-04-14 09:11:24 -0700

    added skeleton to things which arent magic jina edition and also a jina dataclass simple example multi-modal Person

---
+++ scripts/tutorials/oop/composing_nesting_dataclasses_reference.py
@@ -0,0 +1,5 @@
+# https://gist.github.com/ricky-lim/d4ab3ae470d331ead3875f22f6f0580f
+
+#  Composing classes with dataclass, instead of deep-nested dictionary 
+# From the book, effective python 2nd edition item 37
+
--- scripts/tutorials/oop/things_which_arent_magic_reference.py
+++ scripts/tutorials/oop/things_which_arent_magic_reference.py
@@ -8,20 +8,4 @@ class NotFlask:
 
 
 if __name__ == "__main__":
-    # nums = [1, 3, 5, 6]
-    nums = [1, 3, 5, 6]
-    target = 5
-
-    if target < nums[0]:
-        print(f"returning {0}")
-        # return 0
-    elif target > nums[-1]:
-        print(f"returing {len(nums)}")
-        # return len(nums)
-
-    for i in range(len(nums)):
-        if nums[i] == target:
-            print(f'target found: {nums[i]}@{i}')
-            # return i
-        elif nums[i] > target:
-            print(i-1)
+    pass
